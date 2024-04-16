eb969656c92855251db7341525aac7de409f7d35
Sirothpech <christophe.ngan@gmail.com>
2024-04-16 14:45:55 +0200

    Add initial webpack configuration, TypeScript setup, and StudentClass implementation

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -14,3 +14,30 @@ interface Directors extends Teacher {
 function printTeacher(firstName: string, lastName: string): string {
 	return `${firstName.charAt(0)}. ${lastName}`
 }
+
+export interface StudentConstructor {
+	new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+export interface StudentClassInterface {
+	workOnHomework(): string;
+	displayName(): string;
+}
+
+export class StudentClass implements StudentClassInterface {
+	firstName: string;
+	lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	workOnHomework(): string {
+		return 'Currently working';
+	}
+
+	displayName(): string {
+		return this.firstName;
+	}
+}
