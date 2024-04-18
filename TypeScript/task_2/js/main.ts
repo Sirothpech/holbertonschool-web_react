f9287d031997eecb2b334ad7c560c0ae8c57deef
Sirothpech <christophe.ngan@gmail.com>
2024-04-18 09:42:22 +0200

    Moved crud.d.ts, crud.js, and interface.ts to the task_3 directory

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -1,17 +1,65 @@
-/// <reference path="crud.d.ts" />
+interface DirectorInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workDirectorTasks(): string;
+}
 
-import { RowID } from "./interface";
-import { RowElement } from "./interface";
-import * as CRUD from "./crud";
+interface TeacherInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workTeacherTasks(): string;
+}
 
-const row: RowElement = {
-	firstName: 'Guillaume',
-	lastName: 'Salva',
-};
+export const Director = class Director implements DirectorInterface {
+	workFromHome(): string {
+		return 'Working from home';
+	}
+	getCoffeeBreak(): string {
+		return 'Getting a coffee break';
+	}
+	workDirectorTasks(): string {
+		return 'Getting to director tasks';
+	}
+}
 
-const newRowID: RowID = CRUD.insertRow(row);
+export const Teacher = class Teacher implements TeacherInterface {
+	workFromHome(): string {
+		return 'Cannot work from home';
+	}
+	getCoffeeBreak(): string {
+		return 'Cannot have a break';
+	}
+	workTeacherTasks(): string {
+		return 'Getting to work';
+	}
+	
+}
 
-const updatedRow: RowElement = { ...row, age: 23 };
+export function createEmployee(salary: number | string): DirectorInterface | TeacherInterface {
+	if (typeof salary === 'number' && salary < 500) {
+		return new Teacher();
+	}
+	
+	return new Director();
+}
+export function isDirector(employee: DirectorInterface | TeacherInterface): employee is DirectorInterface {
+	return (employee as DirectorInterface).workDirectorTasks !== undefined;
+}
 
-CRUD.updateRow(newRowID, updatedRow);
-CRUD.deleteRow(newRowID);
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+	if(isDirector(employee)) {
+		return employee.workDirectorTasks();
+	} else {
+		return employee.workTeacherTasks();
+	}
+}
+
+export type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects): string {
+	if(todayClass === 'Math') {
+		return 'Teaching Math'
+	} else if (todayClass === 'History') {
+		return 'Teaching History'
+	}
+}
