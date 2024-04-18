30164b1dec513ce53cf4bc412bafb9247172b7d7
Sirothpech <christophe.ngan@gmail.com>
2024-04-18 09:39:56 +0200

    Added task_3 directory with configuration files and implemented interface, ambient declarations, and main functionality as per task requirements.

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -1,65 +1,17 @@
-interface DirectorInterface {
-	workFromHome(): string;
-	getCoffeeBreak(): string;
-	workDirectorTasks(): string;
-}
+/// <reference path="crud.d.ts" />
 
-interface TeacherInterface {
-	workFromHome(): string;
-	getCoffeeBreak(): string;
-	workTeacherTasks(): string;
-}
+import { RowID } from "./interface";
+import { RowElement } from "./interface";
+import * as CRUD from "./crud";
 
-export const Director = class Director implements DirectorInterface {
-	workFromHome(): string {
-		return 'Working from home';
-	}
-	getCoffeeBreak(): string {
-		return 'Getting a coffee break';
-	}
-	workDirectorTasks(): string {
-		return 'Getting to director tasks';
-	}
-}
+const row: RowElement = {
+	firstName: 'Guillaume',
+	lastName: 'Salva',
+};
 
-export const Teacher = class Teacher implements TeacherInterface {
-	workFromHome(): string {
-		return 'Cannot work from home';
-	}
-	getCoffeeBreak(): string {
-		return 'Cannot have a break';
-	}
-	workTeacherTasks(): string {
-		return 'Getting to work';
-	}
-	
-}
+const newRowID: RowID = CRUD.insertRow(row);
 
-export function createEmployee(salary: number | string): DirectorInterface | TeacherInterface {
-	if (typeof salary === 'number' && salary < 500) {
-		return new Teacher();
-	}
-	
-	return new Director();
-}
-export function isDirector(employee: DirectorInterface | TeacherInterface): employee is DirectorInterface {
-	return (employee as DirectorInterface).workDirectorTasks !== undefined;
-}
+const updatedRow: RowElement = { ...row, age: 23 };
 
-export function executeWork(employee: DirectorInterface | TeacherInterface): string {
-	if(isDirector(employee)) {
-		return employee.workDirectorTasks();
-	} else {
-		return employee.workTeacherTasks();
-	}
-}
-
-export type Subjects = 'Math' | 'History';
-
-export function teachClass(todayClass: Subjects): string {
-	if(todayClass === 'Math') {
-		return 'Teaching Math'
-	} else if (todayClass === 'History') {
-		return 'Teaching History'
-	}
-}
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
