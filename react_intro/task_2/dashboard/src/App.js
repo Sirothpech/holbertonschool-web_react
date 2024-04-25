588569a7f0125e01e88a3d2c2f05feabe49fa93c
Sirothpech <christophe.ngan@gmail.com>
2024-04-25 13:38:24 +0200

    Update styling and add new files for React Intro task

--- react_intro/task_2/dashboard/src/App.js
+++ react_intro/task_2/dashboard/src/App.js
@@ -11,9 +11,9 @@ function App() {
       </header>
         <div className="App-body">
         <p>Login to access the full dashboard</p>
-        <label htmlFor='email'>Email</label>
+        <label htmlFor='email'>Email:</label>
         <input type="email" id="email" name="email" />
-        <label htmlFor='password'>Password</label>
+        <label htmlFor='password'>Password:</label>
         <input type="password" id="password" name="password" />
         <button>OK</button>
         </div>
--- react_intro/task_2/dashboard/src/Notifications.js
+++ react_intro/task_2/dashboard/src/Notifications.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import './Notifications.css'
+import './Notifications.css';
+import closeIcon from './close.png';
+import { getLatestNotification } from './utils';
 
 export default function Notifications() {
   return (
     <div className='Notifications'>
+      <button style={ {position: "absolute", right: "20px"}} aria-label='Close'>
+        <img src={closeIcon} alt="close-icon"></img>
+      </button>
         <p>Here is the list of notifications</p>
+        <ul>
+          <li data-priority="default">New course available</li>
+          <li data-priority="urgent">New resume available</li>
+          <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+        </ul>
     </div>
   );
 }
