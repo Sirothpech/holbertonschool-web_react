d3322e749f54dadd04f8e8f8ecf0135e1f4486c3
Sirothpech <christophe.ngan@gmail.com>
2024-04-29 13:11:57 +0200

    Update CSS and add new files for React props task 0

---
+++ react_props/task_0/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Notifications.css';
+// import closeIcon from './close.png';
+import { getLatestNotification } from '../utils/utils';
+
+export default function Notifications() {
+  const handleButtonClick = () => {
+    console.log('Close button has been clicked');
+  }
+  return (
+    <div className='Notifications'>
+      <button style={ {position: "absolute", right: "20px"}} aria-label='Close' onClick={handleButtonClick}>
+        {/* <img src={closeIcon} alt="close-icon"></img> */}
+        x
+      </button>
+        <p>Here is the list of notifications</p>
+        <ul>
+          <li data-priority="default">New course available</li>
+          <li data-priority="urgent">New resume available</li>
+          <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+        </ul>
+    </div>
+  );
+}
