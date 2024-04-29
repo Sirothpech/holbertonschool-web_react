7e3081a0872f25d1765d1d17de5d9e3ea671345c
Sirothpech <christophe.ngan@gmail.com>
2024-04-29 13:14:49 +0200

    Add Notifications component to App.js

--- react_props/task_0/dashboard/src/App/App.js
+++ react_props/task_0/dashboard/src/App/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
 import './App.css';
 
 function App() {
   return (
+    <>
+    <Notifications />
     <div className="App">
       <Header />
         <Login />
         <Footer />
     </div>
+    </>
   );
 }
 
