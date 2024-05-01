09fabcc679688e5a3f58a748e37916e73811ec90
Sirothpech <christophe.ngan@gmail.com>
2024-05-01 14:15:41 +0200

    Add new files and update CSS for React props task 4

---
+++ react_props/task_4/dashboard/src/App/App.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+
+describe('App component', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('renders without crashing', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('renders without crashing', () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it ("renders without crashing", () => {
+		const wrapper = shallow(<Footer />);
+		expect(wrapper.exists()).toBe(true);
+	});
+
+  it ("renders without crashing", () => {
+		const wrapper = shallow(<Notifications />);
+		expect(wrapper.exists()).toBe(true);
+	});
+
+  it ("renders without crashing", () => {
+		const wrapper = shallow(<CourseList />);
+		expect(wrapper.exists()).toBe(true);
+	});
+});
---
+++ react_props/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  const content = isHeader ? (
+    textSecondCell === null ? (
+      <th colSpan="2">{textFirstCell}</th>
+    ) : (
+      <>
+        <th>{textFirstCell}</th>
+        <th>{textSecondCell}</th>
+      </>
+    )
+  ) : (
+    <>
+      <td>{textFirstCell}</td>
+      <td>{textSecondCell}</td>
+    </>
+  );
+
+  return <tr>{content}</tr>;
+}
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.string
+};
+
+CourseListRow.defaultProps = {
+  isHeader: false,
+  textSecondCell: null
+};
