1154f416e3178eec52077a897aeb8009f32252e1
Sirothpech <christophe.ngan@gmail.com>
2024-05-08 13:47:06 +0200

    feat: Add WithLogging higher-order component for logging component mount and unmount

---
+++ React_component/task_4/dashboard/src/HOC/WithLogging.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+const WithLogging = WrappedComponent => {
+  class WithLoggingComponent extends Component {
+    componentDidMount() {
+      console.log(`Component ${WrappedComponent.displayName || WrappedComponent.name || 'Component'} is mounted`);
+    }
+
+    componentWillUnmount() {
+      console.log(`Component ${WrappedComponent.displayName || WrappedComponent.name || 'Component'} is going to unmount`);
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  }
+  
+  WithLoggingComponent.displayName = `WithLogging(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+  return WithLoggingComponent;
+};
+
+export default WithLogging;
