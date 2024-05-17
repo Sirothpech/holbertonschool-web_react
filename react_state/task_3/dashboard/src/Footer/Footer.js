1bdff2702daedeae8309e43aba16c738b1550a34
Sirothpech <christophe.ngan@gmail.com>
2024-05-17 12:54:18 +0200

    Add: task 3

---
+++ react_state/task_3/dashboard/src/Footer/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import { getFullYear, getFooterCopy } from '../utils/utils'
+import AppContext from '../App/AppContext'
+
+const styles = StyleSheet.create({
+  AppFooter: {
+    fontStyle: 'italic',
+    textAlign: 'center',
+    paddingTop: '1rem',
+    borderTop: '3px solid #df354b'
+  },
+  contactUs: {
+    marginTop: '15px',
+    textAlign: 'center'
+  }
+});
+
+function Footer() {
+  const { user } = useContext(AppContext);
+  return (
+    <footer className={css(styles.AppFooter)}>
+      <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      {user.isLoggedIn && (
+        <p className={css(styles.contactUs)}>
+          <a href="#">Contact us</a>
+        </p>
+      )}
+    </footer>
+  );
+}
+
+export default Footer;
