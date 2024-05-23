2ccfb43de57a146630d3d399cf50e90b6280efe5
Sirothpech <christophe.ngan@gmail.com>
2024-05-23 14:38:03 +0200

    feat: Add schema and functions for handling notifications

---
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -0,0 +1,27 @@
+import { normalize, schema } from 'normalizr';
+import * as notificationsData from '../../notifications.json';
+
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', {}, {
+  idAttribute: 'guid'
+});
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message
+});
+
+const normalizeData = (notificationsData) => {
+  const notificationSchema = new schema.Array(notification);
+  return normalize(notificationsData, notificationSchema);
+};
+
+const getAllNotificationsByUser = (userId) => {
+  const { entities } = normalizeData(notificationsData.default);
+  const { notifications, messages } = entities;
+
+  return Object.values(notifications)
+    .filter(notification => notification.author === userId)
+    .map(notification => messages[notification.context]);
+};
+
+export { getAllNotificationsByUser, normalizeData };
