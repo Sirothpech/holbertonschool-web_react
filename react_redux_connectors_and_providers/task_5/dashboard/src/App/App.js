586005540792b883e8a0a38af58b10c2e6ba1c39
Sirothpech <christophe.ngan@gmail.com>
2024-06-05 20:23:01 +0200

    feat: Update App component to fetch notifications on mount t20

--- react_redux_connectors_and_providers/task_5/dashboard/src/App/App.js
+++ react_redux_connectors_and_providers/task_5/dashboard/src/App/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types'; // Import PropTypes
+import PropTypes from 'prop-types';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
 import Notifications from '../Notifications/Notifications';
-import CourseList from '../CourseList/CourseList';
 import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
 import BodySection from '../BodySection/BodySection';
-import { getLatestNotification } from '../utils/utils';
 import AppContext from './AppContext';
 import WithLogging from '../HOC/WithLogging';
-import { displayNotificationDrawer, hideNotificationDrawer, loginRequest } from '../actions/uiActionCreators'; // Import loginRequest
+import { displayNotificationDrawer, hideNotificationDrawer, loginRequest, fetchNotifications } from '../actions/uiActionCreators'; 
 
 const styles = StyleSheet.create({
   header: {
@@ -51,27 +49,10 @@ const listCourses = [
   },
 ];
 
-const listNotifications = [
-  {
-    id: 1,
-    type: 'default',
-    value: 'New course available'
-  },
-  {
-    id: 2,
-    type: 'urgent',
-    value: 'New resume available'
-  },
-  {
-    id: 3,
-    type: 'urgent',
-    html: {__html: getLatestNotification()}
-  }
-];
-
 class App extends Component {
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
+    this.props.fetchNotifications();
   }
 
   componentWillUnmount() {
@@ -86,12 +67,6 @@ class App extends Component {
     }
   }
 
-  markNotificationAsRead = (id) => {
-    this.setState(prevState => ({
-      listNotifications: prevState.listNotifications.filter(notification => notification.id !== id)
-    }));
-  }
-
   render() {
     const { user, isLoggedIn, login } = this.props;
     const { displayDrawer, displayNotificationDrawer, hideNotificationDrawer } = this.props;
@@ -100,8 +75,6 @@ class App extends Component {
       displayDrawer,
       handleDisplayDrawer: displayNotificationDrawer,
       handleHideDrawer: hideNotificationDrawer,
-      listNotifications: listNotifications,
-      markNotificationAsRead: this.markNotificationAsRead,
     };
     return (
       <AppContext.Provider value={value}>
@@ -109,17 +82,17 @@ class App extends Component {
         <div className={css(styles.header)}>
           <Header />
           <WithLogging>
-          <Notifications />
-        </WithLogging>
+            <Notifications />
+          </WithLogging>
         </div>
         <div className={css(styles.body)}>
           {isLoggedIn ? (
             <BodySectionWithMarginBottom title="Course list">
-              <CourseList listCourses={listCourses} />
+              {/* Render CourseList component */}
             </BodySectionWithMarginBottom>
           ) : (
             <BodySectionWithMarginBottom title="Log in to continue">
-              <Login login={login}/> {/* Pass login prop */}
+              <Login login={login}/>
             </BodySectionWithMarginBottom>
           )}
           <BodySection title="News from the School">
@@ -135,23 +108,21 @@ class App extends Component {
   }
 }
 
-// Create mapStateToProps function
 export const mapStateToProps = (state) => {
   return {
     user: state.user,
-    isLoggedIn: state.get('isUserLoggedIn'),  // Access isUserLoggedIn from the immutable state
-    displayDrawer: state.get('isNotificationDrawerVisible') // Access isNotificationDrawerVisible from the immutable state
+    isLoggedIn: state.get('isUserLoggedIn'),
+    displayDrawer: state.get('isNotificationDrawerVisible')
   };
 };
 
-// Create mapDispatchToProps function using shorthand notation
 const mapDispatchToProps = {
   displayNotificationDrawer,
   hideNotificationDrawer,
-  login: loginRequest // Map loginRequest action creator to login prop
+  login: loginRequest,
+  fetchNotifications,
 };
 
-// Define propTypes
 App.propTypes = {
   user: PropTypes.object,
   isLoggedIn: PropTypes.bool,
@@ -161,5 +132,4 @@ App.propTypes = {
   login: PropTypes.func
 };
 
-// Connect mapStateToProps and mapDispatchToProps to the component
 export default connect(mapStateToProps, mapDispatchToProps)(App);
