d25b2361e724f58201b02a1f93ddda5371c13954
Sirothpech <christophe.ngan@gmail.com>
2024-06-05 17:43:25 +0200

    chore: Refactor Header component to use Redux for user state management and PropTypes t10

--- react_redux_connectors_and_providers/task_2/dashboard/src/Header/Header.js
+++ react_redux_connectors_and_providers/task_2/dashboard/src/Header/Header.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import logo from '../assets/holberton-logo.jpg';
-import AppContext from '../App/AppContext';
+import { connect } from 'react-redux'; // Import connect
+import PropTypes from 'prop-types'; // Import PropTypes
+import { logOut } from '../actions/authActions'; // Import logOut action creator
 
 const styles = StyleSheet.create({
   appHeader: {
@@ -14,27 +16,46 @@ const styles = StyleSheet.create({
     width: '200px',
     height: 'auto',
   },
+  logoutSection: {
+    marginTop: '15px',
+    textAlign: 'right',
+  },
+  link: {
+    color: '#df354b',
+    textDecoration: 'none',
+    marginLeft: '5px',
+  }
 });
 
-class Header extends Component {
-  static contextType = AppContext;
-
-  render() {
-    const { user, logOut } = this.context;
-    return (
-      <>
-        <header className={css(styles.appHeader)}>
-          <img src={logo} className={css(styles.holbertonLogo)} alt="Holberton Logo" />
-          <h1>School dashboard</h1>
-        </header>
-          {user.isLoggedIn && (
-            <p id="logoutSection" className={css(styles.logoutSection)}>
-              Welcome <strong>{user.email}</strong> (<a href="#" onClick={logOut} className={css(styles.link)}>Log out</a>)
-            </p>
-          )}
-      </>
-    );
-  }
+function Header({ user, logOut }) { // Destructure user and logOut from props
+  return (
+    <>
+      <header className={css(styles.appHeader)}>
+        <img src={logo} className={css(styles.holbertonLogo)} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      {user.isLoggedIn && (
+        <p id="logoutSection" className={css(styles.logoutSection)}>
+          Welcome <strong>{user.email}</strong> (
+          <a href="#" onClick={logOut} className={css(styles.link)}>Log out</a>)
+        </p>
+      )}
+    </>
+  );
 }
 
-export default Header;
+// Create mapStateToProps function
+const mapStateToProps = (state) => {
+  return {
+    user: state.user // Map the user props to the user within the Redux state
+  };
+};
+
+// Define propTypes
+Header.propTypes = {
+  user: PropTypes.object, // Define propTypes for user prop
+  logOut: PropTypes.func // Define propTypes for logOut action creator
+};
+
+// Connect the Header component to mapStateToProps and the logOut action creator
+export default connect(mapStateToProps, { logOut })(Header);
