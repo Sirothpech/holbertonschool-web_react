a9341a93b32975316725c4af30d5b804be82bb07
Sirothpech <christophe.ngan@gmail.com>
2024-06-06 16:01:19 +0200

    feat: Add fileMock.js, Babel configuration, login success JSON, App CSS, course action types, App context, course shape, setup tests, jest config, course action creators, and utils

---
+++ react_redux_connectors_and_providers/task_6/dashboard/config/setupTests.js
@@ -0,0 +1,11 @@
+// import '@testing-library/jest-dom';
+import { configure } from 'enzyme';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
+import util from 'util';
+
+configure({ adapter: new Adapter() });
+Object.defineProperty(global, 'TextEncoder', {
+  value: util.TextEncoder,
+});
+
+import 'jest-environment-jsdom';
